99ec6238ed10f5a168aca76982130562221b5ada
Max Goryunov <maximthehedgehog@yandex.ru>
2026-05-05 16:56:19 +0300

    change logging format; add logging docs

--- app/core/logging.py
+++ app/core/logging.py
@@ -5,6 +5,6 @@ import sys
 def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        format="%(levelname)s | %(asctime)s | %(name)s | %(message)s",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
