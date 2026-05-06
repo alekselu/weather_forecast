8a521ac0be72d99d1f4bab94d917292fb5b17d8b
eagerbeaver04 <mike.markov04@gmail.com>
2026-05-06 23:18:14 +0300

    add endpoint /forecast

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from typing import Any
+from fastapi import FastAPI, Query
+from typing import Any, Annotated, Dict
 from app import db
+from datetime import date
 from app.core.logging import setup_logging
 
 setup_logging()
@@ -20,3 +21,14 @@ def root():
 def check_db() -> dict[str, Any]:
     result: db.DatabaseHealthResponse = db.check_db()
     return result.to_dict()
+
+
+# Example: GET /forecast?time=2026-05-06&params=temperature&params=humidity&params=wind
+@app.get("/forecast")
+def get_forecast(
+    time: Annotated[date, Query()], params: list[str] = Query()
+) -> dict[str, Any]:
+    result: Dict[str, Any] = {}
+    result["time"] = time
+    result["params"] = params
+    return result
