322d8d4fc30c0200c336c312134e6a38974b4c48
eagerbeaver04 <mike.markov04@gmail.com>
2026-05-07 01:01:27 +0300

    simple mock frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Any, Annotated, Dict
 from app import db
 from datetime import date
@@ -10,6 +11,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+origins = ["http://localhost:3000", "http://localhost:5173"]  # Example: React app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
